fc56d98da7df6639fb48a15729fb1a0b966e9a9d
xz2023 <127741369+xz2023@users.noreply.github.com>
2023-04-29 13:26:21 +0800

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "Munger",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Munger",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
